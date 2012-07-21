44e38a1899ead4d5911071595ebdef97d26f3fff
Maxim Avanov <maxim.avanov@gmail.com>
2012-07-21 21:23:42 +0400

    Add a trivial babel message extractor plugin.

---
+++ tests/adapters/test_babelplugin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from plim.adapters.babelplugin import extract
+from .. import TestCaseBase
+
+
+
+class TestBabelPlugin(TestCaseBase):
+
+    def test_babel_extractor(self):
+        fileobj = self.get_file_contents('babelplugin_test.html')
+        keywords = ['_', 'gettext', 'ungettext', 'pluralize']
+        extracted = [(data[1], data[2]) for data in extract(fileobj, keywords, None, {})]
+        
+        assert ('_', 'Test') in extracted
+        assert ('_', 'View more') in extracted
+
+        assert ('pluralize', ('${num} conversation has been marked as read.',
+                              '${num} conversations have been marked as read.',
+                              None, None)) in extracted
+        assert ('ungettext', ('{num} conversation has been marked as read.',
+                              '{num} conversations have been marked as read.',
+                              None)) in extracted
+        
+        assert ('gettext', 'N') not in extracted
+
+
