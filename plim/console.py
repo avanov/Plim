32c87c0be4afc9a00b21657733e0f60e67aa4de5
Maxim Avanov <maxim.avanov@gmail.com>
2013-07-28 15:15:41 +0400

    proposed modification of plimc

--- plim/console.py
+++ plim/console.py
@@ -1,63 +1,47 @@
-from .lexer import compile_plim_source
-
-from mako.template import Template
-
+"""
+This module contains entry points for command-line utilities provided by Plim package.
+"""
 import sys
-import os.path
+import os
 import argparse
 import codecs
+from pkg_resources import get_distribution
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+from .lexer import compile_plim_source
 
 
-def main():
+def plimc():
     """This is the `plimc` command line utility"""
-    args = _parse_arguments()
-
-    encoding = args.encoding
-    content = _compile_to_mako(args.source, encoding=encoding)
-
-    if args.html:
-        _add_source_folder_path_for_import(args.source)
-        content = _render_mako_to_html(content)
-
-    _write_output(content, args.output, encoding)
-
-
-def _parse_arguments():
-    cli_parser = argparse.ArgumentParser(
-        description='Compile plim source files into mako files.'
-    )
-    cli_parser.add_argument('source',
-                            help="path to source plim template")
-    cli_parser.add_argument('-o', '--output',
-                            help="write result to FILE.")
-    cli_parser.add_argument('-e', '--encoding', default='utf-8',
-                            help="content encoding")
-    cli_parser.add_argument('-H', '--html', action='store_true',
-                            help="Render HTML output instead of Mako template")
-    #cli_parser.add_argument('-v', '--version',
-                            #action='version',
-                            #version='Plim {}'.format('0.8.1'))
+    # Parse arguments
+    # ------------------------------------
+    cli_parser = argparse.ArgumentParser(description='Compile plim source files into mako files.')
+    cli_parser.add_argument('source', help="path to source plim template")
+    cli_parser.add_argument('-o', '--output', help="write result to FILE.")
+    cli_parser.add_argument('-e', '--encoding', default='utf-8', help="content encoding")
+    cli_parser.add_argument('-H', '--html', action='store_true', help="Render HTML output instead of Mako template")
+    cli_parser.add_argument('-V', '--version', action='version',
+                            version='Plim {}'.format(get_distribution("Plim").version))
     args = cli_parser.parse_args()
-    return args
 
+    # Get mako source
+    # ------------------------------------
+    with codecs.open(args.source, 'rb', args.encoding) as fd:
+        content = compile_plim_source(fd.read())
 
-def _write_output(content, target=None, encoding='utf-8'):
-    if target:
-        with codecs.open(target, 'wb', encoding) as fd:
-            fd.write(content)
-    else:
-        print(content)
+    # Get html source if requested
+    # ------------------------------------
+    if args.html:
+        root_dir = os.path.dirname(os.path.abspath(args.source))
+        lookup = TemplateLookup(directories=[root_dir])
+        content = Template(content, lookup=lookup)
 
-
-def _compile_to_mako(source, encoding='utf-8'):
-    with codecs.open(source, 'rb', encoding) as fd:
-        mako_markup = compile_plim_source(fd.read())
-    return mako_markup
-
-
-def _add_source_folder_path_for_import(source):
-    sys.path.append(os.path.dirname(os.path.realpath(source)))
-
-
-def _render_mako_to_html(mako_markup):
-    return Template(mako_markup).render()
+    # Output
+    # ------------------------------------
+    fd = sys.stdout if args.output is None else codecs.open(args.output, 'wb', args.encoding)
+    try:
+        fd.write(content)
+    finally:
+        fd.close()
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'plimc = plim.console:main',
+            'plimc = plim.console:plimc',
         ]
     },
 
