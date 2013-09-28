d98f1d485e922bc89f7a8506fa3341c0b001ae6a
Maxim Avanov <maxim.avanov@gmail.com>
2013-09-28 13:44:18 +0400

    Fix #18.

--- plim/adapters/pyramid_renderer.py
+++ plim/adapters/pyramid_renderer.py
@@ -1,11 +1,21 @@
-from pyramid import mako_templating
-from plim import preprocessor
+try:
+    from pyramid.mako_templating import renderer_factory as mako_renderer_factory
+except ImportError:
+    # Looks like we have Pyramid 1.5a2+
+    try:
+        from pyramid_mako import renderer_factory as mako_renderer_factory
+    except ImportError:
+        raise NotImplementedError(
+            "It seems that you are trying to integrate Plim with Pyramid-1.5a2 or higher. "
+            "To do so, please install the pyramid_mako template bindings."
+        )
 
+from plim import preprocessor
 
 
 def includeme(config):
     config.add_renderer(
-        config.get_settings().get('plim.file_extension', '.plm'),
+        config.get_settings().get('plim.file_extension', '.plim'),
         MakoPlimRenderer
     )
 
@@ -13,7 +23,7 @@ def includeme(config):
 class MakoPlimRenderer(object):
     def __init__(self, info):
         info.settings['mako.preprocessor'] = preprocessor
-        self.makoRenderer = mako_templating.renderer_factory(info)
+        self.makoRenderer = mako_renderer_factory(info)
 
     def __call__(self, value, system):
         return self.makoRenderer(value, system)
