e83821dbc405d8c573583ac5dcacc17bf13b52a0
Maxim Avanov <maxim.avanov@gmail.com>
2013-09-29 13:29:32 +0400

    sys.maxint is not available on Python 3

--- plim/util.py
+++ plim/util.py
@@ -1,7 +1,6 @@
 import sys
 
 
-
 PY3K = sys.version_info >= (3, 0)
 
 if PY3K:
@@ -10,10 +9,12 @@ if PY3K:
     joined = lambda buf: ''.join(buf)
     space_separated = lambda buf: ' '.join(buf)
     as_unicode = str
+    MAXSIZE = sys.maxsize
 
 else:
     from StringIO import StringIO
-    
-    as_unicode = unicode
+
     joined = lambda buf: as_unicode('').join(buf)
     space_separated = lambda buf: as_unicode(' ').join(buf)
+    as_unicode = unicode
+    MAXSIZE = sys.maxint
