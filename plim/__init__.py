c943fab589e05cf928700c321aaed793de96fdca
Maxim Avanov <maxim.avanov@gmail.com>
2014-02-01 01:25:53 +0400

    Improve API #23

--- plim/__init__.py
+++ plim/__init__.py
@@ -3,7 +3,20 @@ import functools
 from .lexer import compile_plim_source, STANDARD_PARSERS
 
 
+def preprocessor_factory(custom_parsers=None):
+    """
+
+    :param custom_parsers: a list of 2-tuples of (parser_regex, parser_callable) or None
+    :type custom_parsers: list or None
+    :return: preprocessor instance
+    """
+    if custom_parsers is None:
+        custom_parsers = []
+    custom_parsers.extend(STANDARD_PARSERS)
+    return functools.partial(compile_plim_source, parsers=tuple(custom_parsers))
+
+
 # ``preprocessor`` is a public object that always follows Mako's preprocessor API.
 # Do not use ``compile_plim_source`` in your projects, because its signature
 # may be changed in the future.
-preprocessor = functools.partial(compile_plim_source, parsers=STANDARD_PARSERS)
+preprocessor = preprocessor_factory()
--- plim/adapters/babelplugin.py
+++ plim/adapters/babelplugin.py
@@ -1,26 +1,35 @@
 """gettext message extraction via Babel: http://babel.edgewall.org/"""
 from mako.ext.babelplugin import extract as _extract_mako
 
-from .. import lexer
+from .. import preprocessor_factory
 from ..util import StringIO, PY3K
 
 
-def extract(fileobj, keywords, comment_tags, options):
-    """Extract messages from Plim templates.
+def extractor_factory(preprocessor=None):
+    if preprocessor is None:
+        preprocessor = preprocessor_factory()
 
-    :param fileobj: the file-like object the messages should be extracted from
-    :param keywords: a list of keywords (i.e. function names) that should be
-                     recognized as translation functions
-    :param comment_tags: a list of translator tags to search for and include
-                         in the results
-    :param options: a dictionary of additional options (optional)
-    :return: an iterator over ``(lineno, funcname, message, comments)`` tuples
-    :rtype: ``iterator``
-    """
-    raw_data = fileobj.read()
-    if not PY3K:
-        encoding = options.get('input_encoding', options.get('encoding', 'utf-8'))
-        raw_data = raw_data.decode(encoding)
-    data = lexer.compile_plim_source(raw_data, parsers=lexer.STANDARD_PARSERS)
-    for extracted in _extract_mako(StringIO(data), keywords, comment_tags, options):
-        yield extracted
+    def babel_extractor(fileobj, keywords, comment_tags, options):
+        """ Extract messages from Plim templates.
+
+        :param fileobj: the file-like object the messages should be extracted from
+        :param keywords: a list of keywords (i.e. function names) that should be
+                         recognized as translation functions
+        :param comment_tags: a list of translator tags to search for and include
+                             in the results
+        :param options: a dictionary of additional options (optional)
+        :return: an iterator over ``(lineno, funcname, message, comments)`` tuples
+        :rtype: ``iterator``
+        """
+        raw_data = fileobj.read()
+        if not PY3K:
+            encoding = options.get('input_encoding', options.get('encoding', 'utf-8'))
+            raw_data = raw_data.decode(encoding)
+        data = preprocessor(raw_data)
+        for extracted in _extract_mako(StringIO(data), keywords, comment_tags, options):
+            yield extracted
+
+    return babel_extractor
+
+# Default Plim extractor
+extract = extractor_factory()
